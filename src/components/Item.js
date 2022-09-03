bb91ae000e20adaeedbbc61deee07a6e3ffcedbe
jonykarpf <jonathankarpf@hotmail.com>
2022-09-03 14:13:28 -0500

    entregable finalizado

---
+++ src/components/Item.js
@@ -0,0 +1,43 @@
+
+// El componente Item no tiene componentes hijos.
+// ESTADO: Item debe tener un número para almacenar la cantidad de stock, la misma se la defina el padre a la hora de crearlo.
+// MÉTODOS: Item debe manejar el click de su boton para restar la cantidad en stock de sí mismo y a su vez poder aumentar el estado de su "abuelo" App.
+// PROPS: Item recibe todos los campos que muestra en pantalla: nombre, descripcion, stock y el métodos heredados para su uso.
+// Maqueta de Item:
+//    h3
+//    p
+//    h5 > span    (este span debe mostrar la cantidad si es mayor a 0 "agotado" si llega a 0)
+//    button       (este boton debe permitir comprar, pero si la cantidad es menor a 0 debe estar deshabilitado y decir "Sin stock")
+
+import React, { useState } from 'react'
+
+export default function Item({ nombre, descripcion, stock, setStock }) {
+
+  const [cantidad, setCantidad] = useState(stock)
+
+  const comprar = () => {
+    if (cantidad > 0) {
+      setCantidad(cantidad - 1)
+      setStock(stock => stock + 1)
+    }
+  }
+
+  return (
+    <div className='producto'>
+      <h3>Zapatillas Nike Revolution 6 Next
+        Nature{nombre}</h3>
+      <p>Suma más y más kilómetros con tus Zapatillas
+        Nike Revolution 6 Next Nature, que aportan amortiquación y contort a cada paso que des
+        Su capellada transpirable y suela diseñada por computadora para una meior tracción son
+        ideales hasta para los corredores más exigentes.{descripcion}</p>
+      <h5>Stock: <span>{cantidad > 0 ? cantidad : 'Agotado'}</span></h5>
+      <button disabled={cantidad <= 0} onClick={comprar}>Comprar</button>
+    </div>
+  )
+}
+
+
+
+
+
+
---
+++ src/components/Listado.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+// Debemos importar el listado de objetos del archivo JSON para usarlos como array a la hora de crear cada Item.
+// El componente Listado es el padre de varios Item
+// ESTADO: Listado no necesita manejar un estado.
+// MÉTODOS: Listado no requiere de métodos.
+// PROPS: Listado recibe el método para aumentar el estado de App y se lo pasa a cada uno de sus hijos.
+
+import Data from './data.json';
+import Item from './Item';
+
+export default function Listado({setStock}) {
+    
+    return (
+      <div className='container'>
+        {Data.map((item, index) => {
+          return (
+            <Item
+              key={index}
+              nombre={item.nombre}
+              descripcion={item.descripcion}
+              stock={item.stock}
+              setStock={setStock}
+            />
+          )
+        })}
+
+      </div>
+    )
+  }
